d31be4878763fa01dbef723ea737e23bb5efb298
Sanka422 <praesidio422@hotmail.com>
2017-12-06 22:48:43 +1100

    init

---
+++ New Unity Project/Assets/AllAssets/Scripts/HexaUnit.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexaUnit : System.IEquatable<HexaUnit> {
+	private HexPosition position;
+	private string name;
+
+	public HexaUnit (HexPosition position, string name) {
+		this.position = position;
+		this.name = name;
+	}
+
+	public HexPosition Position {
+		get {
+			return this.position;
+		}
+		set {
+			position = value;
+		}
+	}
+
+	public string Name {
+		get {
+			return this.name;
+		}
+		set {
+			name = value;
+		}
+	}
+
+	public override int GetHashCode () {
+		return position.GetHashCode () ^ name.GetHashCode ();
+	}
+	
+	/*public override bool Equals (object obj) {
+		if (obj.GetType is HexEntry) {
+			HexEntry entry = (HexEntry) obj;
+			return Position == entry.Position && Name == entry.Name;
+		} else {
+			return false;
+		}
+	}*/
+	
+	public bool Equals (HexaUnit hexaUnit) {
+		return position.Equals (hexaUnit.Position) && name == hexaUnit.Name;
+	}
+}
